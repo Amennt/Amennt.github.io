b37aba729bd30190a96247c2d74eb7a0c6bd878e
Amen't <milosz.koza12@gmail.com>
2022-08-07 23:14:48 +0200

    edit navbar

--- my-react-app/src/components/navbar/Navbar.tsx
+++ my-react-app/src/components/navbar/Navbar.tsx
@@ -7,13 +7,13 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
       <div className='NavbarLogo'>
-        <NavbarLogo logoName='Miłosz Koza' />
+        <NavbarLogo logoName='<Miłosz Koza />' />
       </div>
       <div className='NavbarButtons'>
-        <NavbarButton buttonName='Home' />
-        <NavbarButton buttonName='About' />
-        <NavbarButton buttonName='Skills' />
-        <NavbarButton buttonName='Contact' />
+        <NavbarButton buttonName='.Home()' />
+        <NavbarButton buttonName='.About()' />
+        <NavbarButton buttonName='.Skills()' />
+        <NavbarButton buttonName='.Contact()' />
       </div>
     </div>
   );
